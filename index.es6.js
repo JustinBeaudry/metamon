df50f3803463d4555fe669f5c931dd3c6a360074
Justin Beaudry <beaudry.justin@gmail.com>
2018-06-13 22:18:50 -0700

    adds .mjs export

--- index.es6.js
+++ index.es6.js
@@ -1,4 +1,11 @@
-export Model from './src/Model';
-export Collection from './src/Collection';
-export Enum from './src/Enum';
-export Serializable from './src/Serializable';
+import Model from './src/Model';
+import Collection from './src/Collection';
+import Enum from './src/Enum';
+import Serializable from './src/Serializable';
+
+export default {
+  Model,
+  Collection,
+  Enum,
+  Serializable
+}
---
+++ index.mjs
@@ -0,0 +1,4 @@
+export Model from './src/Model';
+export Collection from './src/Collection';
+export Enum from './src/Enum';
+export Serializable from './src/Serializable';
